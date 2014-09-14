ecc00651dd6ea7b592ad4fef03b41994573e798c
ChiperSoft <chiper@chipersoft.com>
2014-09-13 17:23:20 -0700

    Initial commit

---
+++ tests/bucket.js
@@ -0,0 +1,188 @@
+var Bucket = require('../lib/bucket.js');
+var events = require('events');
+
+exports['emits to queue'] = function (test) {
+	var b = new Bucket();
+	b.emit('testevent', 'arg1', 'argtwo');
+
+	test.deepEqual(b._queue, [['testevent', 'arg1', 'argtwo']]);
+
+	test.done();
+};
+
+exports['multiple emits to queue'] = function (test) {
+	var b = new Bucket();
+	b.emit('testevent', 'arg1', 'argtwo');
+	b.emit('onemore', 2, {foo:'bar'});
+
+	test.deepEqual(b._queue, [['testevent', 'arg1', 'argtwo'], ['onemore', 2, {foo:'bar'}]]);
+
+	test.done();
+};
+
+exports['drains into callback'] = function (test) {
+	var b = new Bucket();
+	b.emit('testevent', 'arg1', 'argtwo');
+	b.emit('onemore', 2, {foo:'bar'});
+
+	b.drain(function (events) {
+		test.deepEqual(events, [['testevent', 'arg1', 'argtwo'], ['onemore', 2, {foo:'bar'}]]);
+
+		test.done();
+	});
+	
+};
+
+exports['drains into promise'] = function (test) {
+	var b = new Bucket();
+	b.emit('testevent', 'arg1', 'argtwo');
+	b.emit('onemore', 2, {foo:'bar'});
+
+	b.drain().then(function (events) {
+		test.deepEqual(events, [['testevent', 'arg1', 'argtwo'], ['onemore', 2, {foo:'bar'}]]);
+
+		test.done();
+	});
+	
+};
+
+exports['drain emits _drained'] = function (test) {
+	var b = new Bucket();
+	b.emit('one');
+	b.emit('two');
+	b.emit('three');
+
+	var emitted = 0;
+
+	b.once('_drained', function () {
+		emitted++;
+		test.strictEqual(emitted, 2);
+
+		test.done();
+	});
+
+	b.drain(function () {
+		emitted++;
+		test.strictEqual(emitted, 1);
+
+	});
+};
+
+
+exports['drains async'] = function (test) {
+	var b = new Bucket();
+	b.drain(function (events) {
+		test.deepEqual(events, [['testevent', 'arg1', 'argtwo'], ['onemore', 2, {foo:'bar'}]]);
+
+		test.done();
+	});
+
+	b.emit('testevent', 'arg1', 'argtwo');
+
+	b.emit('onemore', 2, {foo:'bar'});
+	
+};
+
+exports['drains async, with pause'] = function (test) {
+	test.expect(2);
+
+	var b = new Bucket({pause: 10, maxPause: 50});
+	b.drain(function (events) {
+		test.deepEqual(events, [['testevent', 'arg1', 'argtwo'], ['onemore', 2, {foo:'bar'}]]);
+
+	});
+
+	b.emit('testevent', 'arg1', 'argtwo');
+
+	b.emit('onemore', 2, {foo:'bar'});
+
+	setTimeout(function () {
+		b.emit('three');
+
+		b.drain(function (events) {
+			test.deepEqual(events, [['three']]);
+			test.done();
+		});
+	}, 100);
+	
+};
+
+exports['drains async, with max wait'] = function (test) {
+	test.expect(2);
+
+	var b = new Bucket({pause: 100, maxPause: 500});
+
+	b.drain(function (events) {
+		test.deepEqual(events, [
+			['iteration', 10],
+			['iteration', 9],
+			['iteration', 8],
+			['iteration', 7],
+			['iteration', 6],
+			['iteration', 5]
+		]);
+
+	});
+
+	var count = 10;
+	function loop () {
+		b.emit('iteration', count--);
+
+		if (count > 0) setTimeout(loop, 90);
+	}
+	loop();
+
+	setTimeout(function () {
+		b.drain(function (events) {
+			test.deepEqual(events, [
+				['iteration', 4],
+				['iteration', 3],
+				['iteration', 2],
+				['iteration', 1]
+			]);
+			test.done();
+		});
+	}, 1000);
+
+	
+};
+
+exports['drains into non-function'] = function (test) {
+	test.expect(4);
+
+	var b = new Bucket({pause: 10, maxPause: 50});
+
+	b.emit('testevent', 'arg1', 'argtwo');
+
+	b.emit('onemore', 2, {foo:'bar'});
+
+	var emitter = new events.EventEmitter();
+	emitter.on('testevent', function (arg1, arg2) {
+		test.strictEqual(arg1, 'arg1');
+		test.strictEqual(arg2, 'argtwo');
+	});
+	emitter.on('onemore', function (arg1, arg2) {
+		test.strictEqual(arg1, 2);
+		test.deepEqual(arg2, {foo: 'bar'});
+	});
+	emitter.on('three', function () {
+		test.ok(false, 'should not have received third event');
+	});
+
+	emitter.on('_drained', test.done);
+	
+	b.tap(emitter);
+
+	b.emit('three');
+};
+
+exports['tap error'] = function (test) {
+	var b = new Bucket();
+	b.emit('testevent', 'arg1', 'argtwo');
+
+	test.throws(function () {
+		b.tap(false);
+	});
+
+	test.done();
+};
---
+++ tests/server.js
@@ -0,0 +1,160 @@
+var longEmitter = require('../lib/server.js');
+
+exports['initializes correctly'] = function (test) {
+	var server = longEmitter();
+
+	test.deepEqual(Object.keys(server), ['create','get','close','finish','param','query', 'clear']);
+
+	test.done();
+	server.clear(); //cleanup so our tests don't hang
+};
+
+exports['create bucket'] = function (test) {
+	var server = longEmitter();
+
+	var a = server.create();
+	var id = a.id;
+
+	test.ok(a);
+	test.ok(id);
+
+	var b = server.get(id);
+
+	test.strictEqual(a, b);
+
+	test.done();
+	server.clear(); //cleanup so our tests don't hang
+};
+
+exports['close bucket'] = function (test) {
+	var server = longEmitter();
+
+	var a = server.create();
+	var id = a.id;
+
+	test.ok(a);
+	test.ok(id);
+
+	server.close(id);
+
+	test.ok(!server.get(id));
+
+	test.done();
+	server.clear(); //cleanup so our tests don't hang
+};
+
+exports['bucket cleanup'] = function (test) {
+	var server = longEmitter({
+		expiration: 90,
+		cleanupPeriod: 50
+	});
+
+	var a = server.create();
+	var id = a.id;
+
+	test.ok(a);
+	test.ok(id);
+
+	setTimeout(function () {
+		var b = server.get(id);
+		test.ok(!b, 'Bucket did not expire');
+		test.done();
+		server.clear(); //cleanup so our tests don't hang
+	}, 125);
+};
+
+exports['bucket finished'] = function (test) {
+	var server = longEmitter({
+		expiration: 90,
+		cleanupPeriod: 50
+	});
+
+	var a = server.create();
+	var id = a.id;
+
+	server.finish(id);
+
+	test.ok(server.get(id), 'I lost mah bucket.');
+
+	a.drain();
+
+	a.once('_drained', function () {
+		test.ok(!server.get(id), 'Bucket still exists');
+
+		test.done();
+
+		server.clear(); //cleanup so our tests don't hang
+	});
+
+};
+
+exports['param middleware'] = function (test) {
+	var server = longEmitter();
+
+	var a = server.create();
+
+	a.emit('one');
+	a.emit('two');
+	a.emit('three');
+
+	var res = {locals: {}};
+
+	server.param({}, res, function (err) {
+		test.ok(!err);
+		test.deepEqual(res, {locals: {events: [['one'],['two'],['three']]}});
+		test.done();
+
+		server.clear();
+	}, a.id);
+};
+
+exports['query middleware'] = function (test) {
+	var server = longEmitter({
+		query: 'request'
+	});
+
+	var a = server.create();
+
+	a.emit('one');
+	a.emit('two');
+	a.emit('three');
+
+	var req = {query: {request: a.id}};
+	var res = {locals: {}};
+
+	server.query(req, res, function (err) {
+		test.ok(!err);
+		test.deepEqual(res, {locals: {events: [['one'],['two'],['three']]}});
+		test.done();
+
+		server.clear();
+	});
+};
+
+
+exports['query route'] = function (test) {
+	var server = longEmitter({
+		query: function (req) {
+			return req.query.request;
+		},
+		querySend: true
+	});
+
+	var a = server.create();
+
+	a.emit('one');
+	a.emit('two');
+	a.emit('three');
+
+	var req = {query: {request: a.id}};
+	var res = {locals: {}, json: function (body) {
+		test.deepEqual(body, [['one'],['two'],['three']]);
+		test.done();
+
+		server.clear();
+	}};
+
+	server.query(req, res, function (err) {
+		test.ok(false, 'Should not have called next()');
+	});
+};
